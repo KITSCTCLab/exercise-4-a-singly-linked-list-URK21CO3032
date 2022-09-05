90fe80df4b30c2244d12701987b8058994afdb45
URK21CO3032 <110015264+URK21CO3032@users.noreply.github.com>
2022-09-05 23:21:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -77,3 +77,15 @@ solution = Solution()
 new_list = solution.addTwoNumbers(first_list, second_list)
 # Display the status of new_list
 new_list.status()
+# Read an integer that denotes the length of the list which is returned as the output of the algorithm
+length_of_circular_linked_list = int(input())
+# Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
+circular_linked_list = list(map(int,input().strip().split(" ")))
+# Write your code here
+final_list = [circular_linked_list[i] for i in range(3)]
+for i in circular_linked_list:
+    if i not in final_list:
+        final_list.append(i)
+print(len(final_list))
+for i in final_list:
+    print(i,end=" ")
